8c3de932809408b9ab9e73ffac6d243c1426c807
Ben Zittlau <ben@zittlau.ca>
2013-07-28 22:16:53 -0600

    Some additional tweaks to the config.

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -20,13 +20,13 @@ Streamer::Application.configure do
   # config.action_dispatch.rack_cache = true
 
   # Disable Rails's static asset server (Apache or nginx will already do this).
-  config.serve_static_assets = false
+  config.serve_static_assets = true
 
   config.assets.enabled = true
   config.assets.initialize_on_precompile = false
 
   # Compress JavaScripts and CSS.
-  config.assets.js_compressor = :uglifier
+  # config.assets.js_compressor = :uglifier
   # config.assets.css_compressor = :sass
 
   # Do not fallback to assets pipeline if a precompiled asset is missed.
